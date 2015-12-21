d07d3342767181719d55097e6300b5172b2174f2
Benjamin Brook <benjaminbrook@college.harvard.edu>
2015-12-21 01:39:11 -0500

    Initial Commit

---
+++ bpm/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  bpm
+//
+//  Created by Ben Brook on 2015-12-21.
+//  Copyright © 2015 Ben Brook. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
