9dd130a07523828dd048429fc8109a7077b34cd9
Benjamin Brook <benjaminbrook@college.harvard.edu>
2016-02-28 17:56:41 -0500

    Initial Commit

--- bpm/AppDelegate.swift
+++ bpm/AppDelegate.swift
@@ -13,15 +13,39 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        statusItem.title = "bpm"
+        if let button = statusItem.button {
+            button.action = Selector("clicked:")
+        }
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    
+    var lastPress = NSDate()
+    var avg = 0.0
+    var i = 1.0
+    var timer = NSTimer()
+    
+    func clicked(sender: AnyObject?) {
+        timer.invalidate()
+        let elapsedTime = NSDate().timeIntervalSinceDate(lastPress)
+        if elapsedTime > 2.5 {
+            avg = 0.0
+            i = 1.0
+            lastPress = NSDate()
+        } else {
+            avg = (avg*(i-1) + elapsedTime) / i
+            lastPress = NSDate()
+            i = i + 1
+            let x = Int(60/avg)
+            statusItem.title = String(x)
+        }
+        timer = NSTimer.scheduledTimerWithTimeInterval(2.5, target:self, selector: Selector("updateCounter"), userInfo: nil, repeats: false)
     }
-
-
+    
+    func updateCounter() {
+        statusItem.title = "bpm"
+    }
+    
 }
-
