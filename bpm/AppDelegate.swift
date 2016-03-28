83126d5ae7c48f3e4cc8b488551900a687933536
Benjamin Brook <benjaminbrook@college.harvard.edu>
2016-03-28 16:18:23 -0400

    Version 1.5 with Quit Option

--- bpm/AppDelegate.swift
+++ bpm/AppDelegate.swift
@@ -19,7 +19,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusItem.title = "bpm"
         if let button = statusItem.button {
             button.action = Selector("clicked:")
+            button.keyEquivalent = ""
+            
         }
+        let answer = dialogOKCancel("Instructions", text: ("Control-Click to quit the app.\nTap to calculate BPM.\n\n\nBuilt by Ben Brook:   www.builtbybenbrook.com"))
     }
     
     var lastPress = NSDate()
@@ -27,21 +30,50 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var i = 1.0
     var timer = NSTimer()
     
-    func clicked(sender: AnyObject?) {
-        timer.invalidate()
-        let elapsedTime = NSDate().timeIntervalSinceDate(lastPress)
-        if elapsedTime > 2.5 {
-            avg = 0.0
-            i = 1.0
-            lastPress = NSDate()
-        } else {
-            avg = (avg*(i-1) + elapsedTime) / i
-            lastPress = NSDate()
-            i = i + 1
-            let x = Int(60/avg)
-            statusItem.title = String(x)
+    
+    func dialogOKCancel(question: String, text: String) -> Bool {
+        let myPopup: NSAlert = NSAlert()
+        myPopup.messageText = question
+        myPopup.informativeText = text
+        myPopup.alertStyle = NSAlertStyle.WarningAlertStyle
+        myPopup.addButtonWithTitle("OK")
+        let res = myPopup.runModal()
+        if res == NSAlertFirstButtonReturn {
+            return true
         }
-        timer = NSTimer.scheduledTimerWithTimeInterval(2.5, target:self, selector: Selector("updateCounter"), userInfo: nil, repeats: false)
+        return false
+    }
+    
+    func clicked(sender: AnyObject?) {
+        var clickMask: Int = 0
+        let clickEvent = NSApp.currentEvent!  // see what caused calling of the menu action
+            // modifierFlags contains a number with bits set for various modifier keys
+            // ControlKeyMask is the enum for the Ctrl key
+            // use logical and with the raw values to find if the bit is set in modifierFlags
+        clickMask = Int(clickEvent.modifierFlags.rawValue) & Int(NSEventModifierFlags.ControlKeyMask.rawValue)
+        
+        if clickMask != 0 { NSApplication.sharedApplication().terminate(self) } // click with Ctrl pressed
+            
+        // Regular click
+        else {
+            timer.invalidate()
+            let elapsedTime = NSDate().timeIntervalSinceDate(lastPress)
+            print(elapsedTime)
+            if elapsedTime > 2.5 {
+                print("1")
+                avg = 0.0
+                i = 1.0
+                lastPress = NSDate()
+            } else {
+                print("2")
+                avg = (avg*(i-1) + elapsedTime) / i
+                lastPress = NSDate()
+                i = i + 1
+                let x = Int(60/avg)
+                statusItem.title = String(x)
+            }
+            timer = NSTimer.scheduledTimerWithTimeInterval(2.5, target:self, selector: Selector("updateCounter"), userInfo: nil, repeats: false)
+        }
     }
     
     func updateCounter() {
